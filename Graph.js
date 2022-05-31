05fa1fb48aaf1cf6248d085482964292854ef247
Raquel Rodrigues <raquel.mr.rodrigues@gmail.com>
2022-05-31 17:33:10 -0300

    Graph

---
+++ Graph.js
@@ -0,0 +1,27 @@
+class Graph {
+    constructor() {
+        this.adjacencyList = {};
+    }
+
+    addVertex(vertex) {
+        if(!this.adjacencyList[vertexx]) this.adjacencyList[vertex] = [];
+    }
+
+    addEdge(v1, v2) {
+        this.adjacencyList[v1].push(v2);
+        this.adjacencyList[v2].push(v1);
+    }
+
+    removeEdge(vertex1, vertex2) {
+        this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter( v => v !== vertex2);
+        this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter( v => v !== vertex1);
+    }
+
+    removeVertex(vertex) {
+        while(this.adjacencyList[vertex].length) {
+            const adjacentVertex = this.adjacencyList[vertex].pop();
+            this.removeEdge(vertex, adjacentVertex);
+        }
+        delete this, this.adjacencyList[vertex];
+    }
+}
